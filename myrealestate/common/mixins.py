8db43b79cf745a858dba65d9cab1d1540952b034
Phenyo Itumeleng <phenyoitumeleng@gmail.com>
2024-11-23 08:03:34 +0200

    added a mixin and decortor for views requiring user to have a company in the context

--- myrealestate/common/mixins.py
+++ myrealestate/common/mixins.py
@@ -1,5 +1,10 @@
 # mixins.py
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.contrib import messages
+from .utils import getCurrentCompany
 
 class PatchHandlerMixin:
     """
@@ -12,3 +17,25 @@ class PatchHandlerMixin:
                 return self.patch(request, *args, **kwargs)
             return HttpResponseBadRequest('PATCH method not implemented')
         return super().post(request, *args, **kwargs)
+
+class CompanyRequiredMixin(LoginRequiredMixin):
+    """
+    Mixin that adds current company to the view.
+    Must be used with View classes.
+    """
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        try:
+            self.company = getCurrentCompany(request)
+        except PermissionDenied as e:
+            messages.error(request, str(e))
+            self.handle_no_company()
+
+    def handle_no_company(self):
+        """Override this method to customize the no-company behavior"""
+        raise PermissionDenied("No company access")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = self.company
+        return context
