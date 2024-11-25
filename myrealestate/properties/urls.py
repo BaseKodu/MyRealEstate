ffd9da5931133711ba9ceeed495310ef29c722c1
Phenyo Itumeleng <phenyoitumeleng@gmail.com>
2024-11-25 22:10:50 +0200

    Added ability to view estates

--- myrealestate/properties/urls.py
+++ myrealestate/properties/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from myrealestate.properties.views import EstateCreateView
+from myrealestate.properties.views import EstateCreateView, EstateListView
 
 
 app_name = "properties"
 
 urlpatterns = [
     path("new/", EstateCreateView.as_view(), name="create-estate"),
+    path("", EstateListView.as_view(), name="estate-list"),
 ]
