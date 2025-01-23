48cef2b9cc2f2227aa04951f34cb7314ca4042a0
Phenyo Itumeleng <phenyoitumeleng@gmail.com>
2025-01-23 21:06:14 +0200

    Added models for usage in the django admin

--- myrealestate/properties/admin.py
+++ myrealestate/properties/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Estate, Building, Unit, SubUnit, PropertyImage
+
+admin.site.register(Estate)
+admin.site.register(Building)
+admin.site.register(Unit)
+admin.site.register(SubUnit)
+admin.site.register(PropertyImage)
